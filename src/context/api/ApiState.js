5e1efbd6a9924ea91cb6f5011635ffe33298397a
whoamihealay <whoamihealay@gmail.com>
2021-11-08 11:31:14 +0100

    Added Comments to ApiState

--- src/context/api/ApiState.js
+++ src/context/api/ApiState.js
@@ -34,6 +34,8 @@ const ApiState = (props) => {
 
   const [state, dispatch] = useReducer(ApiReducer, initialState);
 
+  // getAll: Sends GET request to API /all endpoint to retrieve all countries in database. Fields to reduce payload size. If more info needed, add fields.
+
   const getAll = async () => {
     setLoading();
 
@@ -47,25 +49,21 @@ const ApiState = (props) => {
     });
   };
 
+  // getCountry: Sends GET to /name/ endpoint with Name of the country as variable from CountryCard Component. Fields to reduce payload size. If more info needed, add fields.
+
   const getCountry = async (countryName) => {
-    try {
-      setLoading();
+    setLoading();
 
-      const res = await axios.get(
-        `https://restcountries.com/v2/name/${countryName}?fields=flags,name,nativeName,population,region,subregion,capital,topLevelDomain,currencies,languages,borders,`
-      );
-
-      dispatch({
-        type: GET_COUNTRY,
-        payload: res.data[0],
-      });
-    } catch (err) {
-      dispatch({
-        type: ERROR,
-        payload: err.response.statusText,
-      });
-    }
+    const res = await axios.get(
+      `https://restcountries.com/v2/name/${countryName}?fields=flags,name,nativeName,population,region,subregion,capital,topLevelDomain,currencies,languages,borders,`
+    );
+    dispatch({
+      type: GET_COUNTRY,
+      payload: res.data[0],
+    });
   };
+
+  // searchCountry: Sends GET to /name/ endpoint with Name of the country as variable from Search Component. Fields to reduce payload size. If more info needed, add fields.
 
   const searchCountry = async (text) => {
     setLoading();
@@ -89,6 +87,8 @@ const ApiState = (props) => {
     }
   };
 
+  // filterRegion: Sends GET to /regionalbloc/ endpoint with Name of the region as variable from Filter Component. Fields to reduce payload size. If more info needed, add fields.
+
   const filterRegion = async (region) => {
     setLoading();
 
@@ -102,7 +102,11 @@ const ApiState = (props) => {
     });
   };
 
+  // setLoading: sets loading to true while GET requets are fullfilled.
+
   const setLoading = () => dispatch({ type: SET_LOADING });
+
+  // clearCountry: clears the Country objet as cleanup. No stricly neccesary but keeps control over action.
 
   const clearCountry = () => dispatch({ type: CLEAR_COUNTRY });
 
